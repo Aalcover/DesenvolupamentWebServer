cdb213bbc5f16794082cc82150fa278bca136f73
Aleix Alcover <clonic79@gmail.com>
2018-11-14 18:27:30 +0100

    Select

---
+++ JSON/Activitat2/select.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+$conn = new mysqli("192.168.1.44", "aleix", "12345", "nautic");
+$stmt = $conn->prepare("SELECT * FROM tarifa");
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($outp);
